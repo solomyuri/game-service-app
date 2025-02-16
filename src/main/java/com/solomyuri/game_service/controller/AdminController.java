3e383b83b683937b27be647e035a04803b92afa6
solomyuri <solomyuri90@gmail.com>
2025-02-16 14:31:46 +0300

    refactor getUser

---
+++ src/main/java/com/solomyuri/game_service/controller/AdminController.java
@@ -0,0 +1,15 @@
+package com.solomyuri.game_service.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("v1/admin")
+public class AdminController {
+
+}
